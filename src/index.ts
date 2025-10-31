1ebf8048ecbd90df92d30b039b03954a8c4d9c9a
seosd97 <seosd97@gmail.com>
2025-10-31 18:05:50 +0900

    tsconfig

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const server = new ApolloServer({
+  typeDefs: `#graphql
+    type Query {
+      hello: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => "Hello, world!",
+    },
+  },
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 8000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
